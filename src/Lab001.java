9d369193ea0d11aa1ddbaf074559571607063ace
Fassica <fasdamc@gmail.com>
2025-06-05 12:57:52 -0400

    second commit

--- src/Lab001.java
+++
@@ -1,5 +0,0 @@
-public class Lab001 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/ex_01_java_basics/Lab001.java
@@ -0,0 +1,7 @@
+package ex_01_java_basics;
+
+public class Lab001 {
+    public static void main(String[] args) { //what JVM runs
+
+    }
+}
